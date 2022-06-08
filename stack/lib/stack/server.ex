ca014a1a0b61c6c8ea59a61f74f12cd7e7ebc593
hj <mmjh9258@gmail.com>
2022-06-08 15:20:22 +0900

    Day6 elixir study stack-clean

--- stack/lib/stack/server.ex
+++ stack/lib/stack/server.ex
@@ -1,6 +1,22 @@
 defmodule Stack.Server do
   use GenServer
 
+  def start_link(current_number) do
+    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+  end
+
+  def push(delta) do
+    GenServer.cast __MODULE__, {:push, delta}
+  end
+
+  def pop do
+    GenServer.call __MODULE__, :pop
+  end
+
+  def set_list(new_list) do
+    GenServer.cast __MODULE__, {:set_list, new_list}
+  end
+
   def init(initial_list) do
     { :ok, initial_list }
   end
@@ -9,10 +25,6 @@ defmodule Stack.Server do
     { :reply, new_list, new_list }
   end
 
-  # def handle_call({:factors, number}, _, _) do
-  #   { :reply, { :factors_of, number, factors(number)}, [] }
-  # end
-
   def handle_call(:pop, _from, current) do
     { head, tail } = List.pop_at(current, -1)
     { :reply, head, tail }
@@ -20,6 +32,10 @@ defmodule Stack.Server do
 
   def handle_cast({:push, delta}, current) do
     { :noreply, current ++ [delta] }
+  end
+
+  def format_status(_reason, [ _pdict, state ]) do
+    [data: [{'State', "My current state is '#{inspect state}', and I'm happy"}]]
   end
 
 end
